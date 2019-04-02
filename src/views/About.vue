24ec83c869e746aaf962909a0c6811cf37c4404e
Gabe Landau <gabelandau14@gmail.com>
2019-04-02 19:09:09 -0400

    Added credit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home content-wrapper animated fadeInUp fast">
     <h2 class="title is-2">About</h2>
-    <p class="subtitle">WirePair was written by me, <a href="https://gabelandau.com">Gabe Landau</a>, as a final project for Wines of the World I, a class taken at RIT.</p>
-    <p class="subtitle">Code for the project is publically available on <a href="http://git.gabelandau.com">my GitHub page</a>.</p>
+    <p class="subtitle">WirePair was written by <a href="https://gabelandau.com">Gabe Landau</a> and Frankie Albin as a final project for Wines of the World I, a class taken at RIT.</p>
+    <p class="subtitle">Code for the project is publically available on <a href="http://git.gabelandau.com">Gabe's GitHub page</a>.</p>
   </div>
 </template>
